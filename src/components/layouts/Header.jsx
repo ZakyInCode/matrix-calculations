d860adfce4ec7cd578e6d99abb15fcebe46f2088
ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
2024-05-29 22:24:26 +0700

    Squashed commit of the following:
    commit 5d8ca18330087aa46686a89066fad5b2a2dc9258
    Merge: 01f0cdf 95023a5
    Author: ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
    Date:   Wed May 29 22:23:24 2024 +0700
    
        Merge branch 'feat' of https://github.com/ZakyInCode/matrix-calculator into feat
    
    commit 01f0cdfd30908bcac3626b07fd5229d55de166a6
    Author: ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
    Date:   Wed May 29 22:22:10 2024 +0700
    
        test new version/v2
    
    commit 95023a559cf8b1a972eea1ec8598f2c587aec145
    Merge: 33564c2 3788b56
    Author: Ahmad Zaky Ubaidillah <ubaidillahzaky11@gmail.com>
    Date:   Sat Mar 30 21:46:43 2024 +0700
    
        Merge branch 'main' into feat
    
    commit 33564c277807635b2b3f308d8597c93ef0f51192
    Author: ahmad zaky ubaidillah <ubaidillahzaky11@gmail.com>
    Date:   Sat Mar 30 21:43:47 2024 +0700
    
        Add Automatic Class Sorting with Prettier
    
    commit 3788b5627cbba93f80080b8b8378e08851b3e925
    Author: Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
    Date:   Mon Nov 13 15:32:17 2023 +0700
    
        Update README.md
    
    commit f1055eb5858008bc89a78b6db54002c61b2376e8
    Author: Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
    Date:   Mon Nov 13 15:10:27 2023 +0700
    
        Update README.md
    
    commit f56edf3c434dcee338f801e95f4751ec7ff983f5
    Author: Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
    Date:   Sat Oct 21 07:33:29 2023 +0700
    
        fix text in preview mode (#23)
    
    commit b67d723e2f7a09e9ec4a377c4c4de93c34bcbe03
    Merge: b131561 26d41b2
    Author: Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
    Date:   Sat Oct 21 07:24:25 2023 +0700
    
        Merge pull request #21 from zakyscripters/dev
    
        feat(dev) from (feat) : new preview mode feature
    
    commit b131561384ba7d1382642936e0f9744e33224c92
    Merge: e37ca02 d211e7a
    Author: Ahmad Zaky Ubaidillah <111162218+zakyscripters@users.noreply.github.com>
    Date:   Thu Oct 5 07:45:08 2023 +0700
    
        Merge pull request #19 from zakyscripters/dev
    
        fix(dev): fix toast

--- src/components/layouts/Header.jsx
+++ src/components/layouts/Header.jsx
@@ -4,33 +4,21 @@ import HeaderLink from "@/src/components/fragments/HeaderLink";
 
 const HeaderPage = ({ title }) => {
   return (
-    <header className="container flex flex-col items-start justify-between w-full h-full px-3 py-2 sm:flex-row sm:items-center bg-slate-800 rounded-b-md">
+    <header className="container flex h-full w-full flex-col items-start justify-between rounded-b-md bg-slate-800 px-3 py-2 sm:flex-row sm:items-center">
       <HeaderLink />
-      <TitleMatrix title={title} />
-      <HomeLinkButton />
+      <h1 className="hidden w-1/3 text-center font-bold uppercase text-slate-100 transition-all sm:block sm:text-2xl md:text-3xl lg:text-4xl">
+        {title}
+      </h1>
+      <div className="flex w-1/3 items-center justify-end">
+        <Link
+          className="hidden rounded-md border-2 border-blue-300 bg-blue-900 px-3 py-0.5 text-center font-semibold text-white hover:bg-blue-700 active:bg-slate-200 sm:block"
+          href={"/"}
+        >
+          Home
+        </Link>
+      </div>
     </header>
   );
 };
 
 export default HeaderPage;
-
-const HomeLinkButton = () => {
-  return (
-    <div className="flex items-center justify-end w-1/3">
-      <Link
-        className="hidden px-3 py-0.5 font-semibold text-center text-white bg-blue-900 border-2 border-blue-300 rounded-md sm:block hover:bg-blue-700 active:bg-slate-200"
-        href={"/"}
-      >
-        Home
-      </Link>
-    </div>
-  );
-};
-
-const TitleMatrix = ({ title }) => {
-  return (
-    <h1 className="hidden w-1/3 font-bold text-center uppercase transition-all sm:block sm:text-2xl md:text-3xl lg:text-4xl text-slate-100">
-      {title}
-    </h1>
-  );
-};
